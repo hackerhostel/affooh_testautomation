7378d38e2eba631124af890340e500e52fa3769f
unknown <rkpsandalidilshani@gmail.com>
2025-07-06 18:00:49 +0530

    add folder structure base

---
+++ pages/login/LoginPage.js
@@ -0,0 +1,19 @@
+exports.LoginPageLoginPage=class LoginPage {
+    constructor(page) {
+        this.page = page;
+        this.usernameInput = page.locator('input[name="username"]');
+        this.passwordInput = page.locator('input[name="password"]');
+        this.loginButton = page.getByRole('button', { name: 'Login' });
+    }
+
+    async navigateToLogin() {
+        await this.page.goto('https://dev-app.affooh.com/login');
+    }
+
+    async login(username, password) {
+        await this.usernameInput.fill(username);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
+    }
+}
+
---
+++ tests/login/login.spec.js
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+import { LoginPageLoginPage } from '../../pages/login/LoginPage.js';
+
+let login;
+test.describe('Login Tests', () => {
+    test.beforeEach(async ({ page }) => {
+        login =new LoginPageLoginPage(page)
+        await login.navigateToLogin();
+    });
+
+    test('valid login', async ({ page }) => {
+        await login.login('csmenike6@gmail.com', 'Chandima@123');
+        await expect(page).toHaveURL('https://dev-app.affooh.com/dashboard');
+    });
+
+    test('invalid login', async ({ page }) => {
+        await login.login('csmenike6@gmail.com', 'invalidPassword');
+        await expect(page.getByText('Incorrect username or password.')).toBeVisible();
+    });
+
+    test('empty fields login', async ({ page }) => {
+        await login.login('', '');
+        await expect(page.getByText('username is a required field')).toBeVisible();
+        await expect(page.getByText('password is a required field')).toBeVisible();
+
+    });
+
+    test('login with only username', async ({ page }) => {
+        await login.login('csmenike6@gmail.com', '');
+        await expect(page.getByText('password is a required field')).toBeVisible();
+    });
+
+    test('login with only password', async ({ page }) => {
+        await login.login('', 'Chandima@123');
+        await expect(page.getByText('username is a required field')).toBeVisible();
+    });
+});
